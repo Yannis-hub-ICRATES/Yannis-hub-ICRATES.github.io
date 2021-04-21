d78a82cf52f5bc8c910d02c39639022049ce9563
Yannis-hub-ICRATES <78437195+Yannis-hub-ICRATES@users.noreply.github.com>
2021-04-21 16:06:36 +0200

    Update to iteration 2

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -11,3 +11,30 @@ function embedPublications() {
     vegaEmbed('#vis3', spec)
         .catch(console.warn);
 }
+
+function embedScatterPlot() {
+    document.getElementById("vis1").innerHTML = ""
+    const spec = "overview_cyprus.vl.json"
+    vegaEmbed('#vis1', spec)
+        .catch(console.warn);
+}
+
+function embedTable() {
+    document.getElementById("vis1").innerHTML = ""
+    document.getElementById("vis1").innerHTML =
+        "<img src= 'images/map_of_cyprus3.png' alt='Overview table' width='475' class='map'/>"
+}
+
+function embedFabricOverview() {
+    document.getElementById("vis2").innerHTML = ""
+    const spec = "fabrics_overview.vl.json"
+    vegaEmbed('#vis2', spec)
+        .catch(console.warn);
+}
+
+function embedSFUncertain() {
+    document.getElementById("vis2").innerHTML = ""
+    const spec = "sherds_uncertain.vl.json"
+    vegaEmbed('#vis2', spec)
+        .catch(console.warn);
+}
